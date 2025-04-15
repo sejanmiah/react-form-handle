202c602e96f83f3d3ea09320ff36c81c2b307b65
sejanmiah <sejankhan931@gmail.com>
2025-04-16 00:23:55 +0400

    Ajker moto ekhainei ses korlam, dekhan hobe arekdin, inshallah

--- src/component/ProductMangement/ProductForm.jsx
+++ src/component/ProductMangement/ProductForm.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductForm = () => {
+const ProductForm = ({ handleAddProduct }) => {
+    const [error, setError] = useState('')
+    const handleProductSubmit = (e) => {
+        e.preventDefault();
+
+        const name = e.target.name.value;
+        const price = e.target.price.value;
+        const quantity = e.target.quantity.value;
+        console.log(name, price, quantity);
+
+        const newProduct = {
+            name,
+            price,
+            quantity
+        }
+        // console.log(newProduct);
+
+
+
+
+        if (name.length === 0) { 
+            setError('Please Provide e Product Name');
+            return;
+        }
+        else if(price < 0){
+            setError('Please Provide a posetive product  name');
+            return
+        }
+        else if(price.lenght === 0) {
+            setError('Plaease provide e product price');
+            return
+        }
+        else{
+            setError('');
+        }
+        
+        handleAddProduct(newProduct);
+    }
     return (
         <div>
-            
+            <h3>Add your Product</h3>
+            <form onSubmit={handleProductSubmit}>
+                <input type="text" name='name' placeholder='Product Er Name' />
+                <br />
+                <input type="text" name='price' placeholder='Product Er Price' />
+                <br />
+                <input type="text" name='quantity' placeholder='Product Er Quantity' />
+                <br />
+                <input type="submit" value="submit" />
+            </form>
+            <p style={{color: 'red'}}><small> {error} </small></p>
         </div>
     );
 };
--- src/component/ProductMangement/ProductTable.jsx
+++ src/component/ProductMangement/ProductTable.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const ProductTable = () => {
+const ProductTable = ({ products }) => {
     return (
         <div>
-            
+            <h3> Product: {products.length} </h3>
+            <table style={{border: '1px solid black'}}>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product, index) => 
+                            <tr key={index}>
+                                <td> {index + 1} </td>
+                                <td> {product.name} </td>
+                                <td> {product.price}  </td>
+                                <td> {product.quantity}  </td>
+                                <td> <button>Buy Now</button> </td>
+                            </tr>
+                         )
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
